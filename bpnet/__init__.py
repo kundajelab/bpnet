325f8a81fba6539b1e5284e09f7699153f664f79
Avsecz <zigaavsec@gmail.com>
2019-08-05 13:33:42 +0200

    Bump version: 0.0.3 → 0.0.4

--- bpnet/__init__.py
+++ bpnet/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __author__ = 'Ziga Avsec'
 __email__ = 'avsec@in.tum.de'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 try:
     from comet_ml import Experiment  # needs to be imported before keras/tensorflow
