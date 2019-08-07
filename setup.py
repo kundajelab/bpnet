352f14ae0df1805264de4d8a9c03f08061d80285
Avsecz <zigaavsec@gmail.com>
2019-08-07 19:22:38 +0200

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ dependency_links = [
 
 setup(
     name="bpnet",
-    version='0.0.10',
+    version='0.0.11',
     description=("BPNet: toolkit to learn motif synthax from high-resolution functional genomics data"
                  " using convolutional neural networks"),
     author="Ziga Avsec",
