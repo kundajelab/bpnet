e81161185cb4d225f76e0eb75852bcb05a7f7387
Vivekanandan Ramalingam <vir@stanford.edu>
2023-03-29 04:11:16 -0700

    add back shap in setup.py; modiscolite is still not part of the setup since it requires numpy 1.21 while tf.2.4 requires numpy 1.19

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/kundajelab/bpnet",
     packages=find_packages(exclude=["docs", "docs-build"]),
     install_requires=["tensorflow==2.4.1", 
-                      "tensorflow-probability==0.12.2", 
+                      "tensorflow-probability==0.12.2", "shap @ git+https://github.com/kundajelab/shap.git",
                       "tqdm", "scikit-learn",
                       "scipy", "scikit-image", "scikit-learn", "deepdish", "pandas", "matplotlib", "plotly", 
                       "deeptools", "pyfaidx", "deeplift", "hdf5plugin"
